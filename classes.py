437064753cb8b13e89c87b581609dd5bb746dd2d
Matthew Du Pont <github@matthewfdupont.com>
2016-11-11 08:28:15 -0800

    input parsing and class initialization

--- classes.py
+++ classes.py
@@ -1,17 +1,36 @@
 #!/usr/bin/env python
 
+#INPUT 
+#grid_list:  list of zeros and hypens
+#piece_list: for default orientation, grid squares filled by a piece
+#
+#CONTENTS
+#grid: array of array of strings. 0 = empty, - = not usable,
+#pieces: array of the pieces valid for this board
+#will be filled with piece ids over time
+#xy_next: [x,y] where placing first piece not in use
+#orient_next: int[0-3] with 0 up, 1 right, etc to orient piece
+#placing_piece: location in array of piece being placed at this call level
 class Board:
-	#input: r_g_a array of zeros and hypens
-	#
-	#grid: array of array of strings. 0 = empty, - = not usable,
-	#will be filled with piece ids over time
-	#xy_next: [x,y] where placing first piece not in use
-	#orient_next: int[0-3] with 0 up, 1 right, etc to orient piece
-	#pieces: array of the pieces valid for this board
-	#placing_piece: location in array of piece being placed at this call level
-	def __init__(self,raw_grid_array = [], *args):
-		self.grid = [list(line) for line in raw_grid_array]
+	def __init__(self,grid_list = [], piece_list = [], *args):
+		self.grid = [list(line) for line in grid_list]
+
+		if len(piece_list) > 100:
+			raise(AssertionError, "Weird stuff is going to happen with your chars")
+
+		self.pieces = []
+		char_num = 65 #ASCII 'A' - want lot of readable chars in a row
+		for line in piece_list:
+			self.pieces.append(Piece(line,char_num))
+			char_num += 1
+
+	def __repr__(self):
+		return "Grid: " + str(self.grid) + "\nPieces: " + str(self.pieces)
 
 class Piece:
-	def __init__(self,raw_piece_array = [], *args):
-		print "hi"
+	def __init__(self,coordinate_list = [], char_num = 65, *args):
+		self.coordinates = coordinate_list
+		self.char = chr(char_num)
+
+	def __repr__(self):
+		return "Char: " + self.char + ", Coordinates: " + str(self.coordinates) + "\n"
--- main.py
+++ main.py
@@ -1,7 +1,42 @@
 #!/usr/bin/env python
 
-from classes import Board 
+from classes import Board
+from copy import deepcopy
 
-sample_board = ["000-0","0---0"]
-b = Board(sample_board)
-print b.grid
+def scenario_parser(filename):
+	file = open(filename)
+	lines = [line.replace("\n","") for line in file.readlines()]
+	grid_input = []
+	pieces = []
+
+	i = 0
+	while (i < len(lines) and lines[i]): #will be false if empty string, new to me
+		grid_input.append(lines[i])
+		i += 1
+	
+	i += 1 #skip the blank string it ended on
+
+	number_of_pieces = int(lines[i])
+	i += 1
+
+	one_piece = []
+	while (i < len(lines)):
+		if lines[i]:
+			one_piece.append(lines[i])
+		else:
+			pieces.append(deepcopy(one_piece))
+			one_piece = []
+		i += 1
+
+	if one_piece:
+		pieces.append(deepcopy(one_piece))
+
+	if len(pieces) != number_of_pieces:
+		print lines
+		print one_piece, len(pieces), number_of_pieces
+		raise(AssertionError, "Mismatched number of pieces")
+
+	return Board(grid_input,pieces)
+
+board = scenario_parser("base.scenario")
+print board
